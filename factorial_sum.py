ac14eef43067be7b675866093fe859fc870bdcd2
Michelangelo <154355759+script0x0kiddy@users.noreply.github.com>
2026-02-03 19:50:29 +0300

    Add files via upload

---
+++ factorial_sum.py
@@ -0,0 +1,27 @@
+# from math import factorial
+
+# print('Сумма факториалов ❗')
+# n = int(input())
+# total = 0
+
+# for i in range(1, n + 1):
+#     total += factorial(i)
+
+# print(total)
+
+print('Сумма факториалов ❗')
+
+n = int(input())
+
+total = 0
+count = 1
+factorial = 1
+
+for i in range(1, n + 1):
+    for f in range(count, i + 1):
+        factorial *= i
+        count += 1
+
+    total += factorial
+
+print(total)
