eb3f42a279ba9f41586a3045f74430af0ae92034
Daudov Rahim <k.kalashnikova0301@gmail.com>
2026-02-16 16:15:02 +0300

    Впервые добавил скрипт через терминал

---
+++ two_parts.py
@@ -0,0 +1,37 @@
+print("Две половинки 🌶️")
+
+from math import ceil
+
+string = str(input())
+left = ""
+right = ""
+
+if len(string) // 2 == 1:
+    for i in range(0, len(string)):
+        if i <= len(string) // 2:
+            left = string[:i]
+        if i >= len(string) // 2:
+            right = string[i:]
+            break
+
+elif len(string) % 2 == 0:
+    for i in range(0, len(string)):
+        if i <= len(string) // 2:
+            left = string[:i]
+        elif i >= len(string) // 2:
+            right = string[i - 1:]
+            break
+
+elif len(string) % 2 != 0:
+    for i in range(0, len(string)):
+        if i <= ceil(len(string) / 2):
+            left = string[:i]
+        elif i >= ceil(len(string) / 2):
+            right = string[i - 1:]
+            break
+
+if left == "" and right == "":
+    left = string
+    print(left)
+else:
+    print(right, left, sep="")
